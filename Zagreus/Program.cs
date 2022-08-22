883e6738f8ec0abb4662b9ef2f9983bd2191cb37
Lindow <das@umbraco.dk>
2022-08-21 23:47:45 -0400

    numeric data types

--- Zagreus/Program.cs
+++ Zagreus/Program.cs
@@ -10,27 +10,39 @@ namespace Zagreus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Zagreus");
+            bool isHadesAGoodGame = true;
 
-            for (int i = 0; i < args.Length; i++)
-            {
-                Console.WriteLine("Char {0} : {1}", i, args[i]);
-            }
-             
-            string[] myChars = Environment.GetCommandLineArgs();
-            Console.WriteLine(string.Join(", ", myChars));
+            decimal decPiVal = 3.1415926535897932384626433832M;
+            decimal decBigNum = 3.0000000000000000000000000011M;
 
-            ReturnChar();
+
+            Console.WriteLine("DEC : PI + bigNum {0}", decPiVal + decBigNum);
+
+            double dblPiVal = 3.14159265358979;
+            double dblBigNum = 3.00000000000002;
+
+            Console.WriteLine("DBL : PI + bigNum {0}", dblPiVal + dblBigNum);
+
+            float fltPiVal = 3.141592F;
+            float fltBigNum = 3.000002F;
+
+            Console.WriteLine("FLT : PI + bigNum {0}", fltPiVal + fltBigNum);
+
+
+            Console.WriteLine("Biggest int : {0}", int.MaxValue);
+            Console.WriteLine("Smallest int : {0}", int.MinValue);
+            Console.WriteLine("Biggest long : {0}", long.MaxValue);
+            Console.WriteLine("Smallest long : {0}", long.MinValue);
+            Console.WriteLine("Biggest dec : {0}", decimal.MaxValue);
+            Console.WriteLine("Smallest dec : {0}", decimal.MinValue);
+            Console.WriteLine("Biggest dbl : {0}", double.MaxValue.ToString("#"));
+            Console.WriteLine("Smallest dbl : {0}", double.MinValue.ToString("#"));
+            Console.WriteLine("Biggest flt : {0}", float.MaxValue.ToString("#"));
+            Console.WriteLine("Smallest flt : {0}", float.MinValue.ToString("#"));
+
 
             Console.ReadLine(); 
         }
 
-        private static void ReturnChar()
-        {
-            string name = "";
-            Console.Write("Who is your favorite character? ");
-            name = Console.ReadLine();
-            Console.WriteLine("Your favorite character is {0}", name);
-        }
     }
 }
