6905b8d41be1c4e0746743f1b02d6da5aab67709
Lindow <das@umbraco.dk>
2022-08-21 20:24:36 -0400

    initial

---
+++ Zagreus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zagreus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zagreus");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Char {0} : {1}", i, args[i]);
+            }
+             
+            string[] myChars = Environment.GetCommandLineArgs();
+            Console.WriteLine(string.Join(", ", myChars));
+
+            Console.ReadLine(); 
+        }
+    }
+}
