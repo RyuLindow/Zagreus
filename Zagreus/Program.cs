90bb215c42468294ccb81ad3a08b108942a70ef8
Lindow <das@umbraco.dk>
2022-08-25 22:13:26 -0400

    2 foreach arrays

--- Zagreus/Program.cs
+++ Zagreus/Program.cs
@@ -23,10 +23,35 @@ namespace Zagreus
             Console.WriteLine("1 at Index : {0}",
                 Array.IndexOf(rndNmbrs, 1));
 
-
             PrintArray(rndNmbrs, "ForEach");
 
+            int[] srcArray = { 10, 02, 95 };
+            int[] destArray = new int[2];
+            int startIndx = 0;
+            int length = 2;
 
+            Array.Copy(srcArray, startIndx, destArray, startIndx, length);
+
+            PrintArray(destArray, "Copy");
+
+            Array nthrArray = Array.CreateInstance(typeof
+                (int), 10);
+
+            srcArray.CopyTo(nthrArray, 5);
+
+            foreach (int i in nthrArray)
+            {
+                Console.WriteLine("CopyTo : {0}", i);
+            }
+
+            int[] numArray = { 10, 17, 06 };
+
+            Console.WriteLine("> 10 : {0}", Array.Find(numArray, grtrThn10));
+
+
+
+
+            Console.ReadLine();
         }
 
         static void PrintArray(int[] intArray, string msg)
@@ -35,8 +60,11 @@ namespace Zagreus
             {
                 Console.WriteLine( "{0} : {1} ", msg, n);
             }
+        }
 
-            Console.ReadLine();
+        private static bool grtrThn10(int val)
+        {
+            return val > 10;
         }
 
     }
