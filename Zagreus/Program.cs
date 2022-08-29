42f11fc288da201d2bed89c7a5970cf78e2fcfb2
Lindow <das@umbraco.dk>
2022-08-28 21:42:44 -0400

    3 and or operators

--- Zagreus/Program.cs
+++ Zagreus/Program.cs
@@ -12,9 +12,36 @@ namespace Zagreus
     {
         static void Main(string[] args)
         {
-            
-            Relation Operators : > < => <= == !=
-            Logical Operators : &&(and) ||(or) !(not)
+
+            int age = 12;
+
+            if ((age >= 26) && (age <= 43))
+            {
+                Console.WriteLine("Millenial");
+            }
+            else if ((age > 43) && (age <= 59))
+            {
+                Console.WriteLine("Gen X");
+            }
+            else if ((age > 59) && (age <= 77))
+            {
+                Console.WriteLine("Boomer");
+            }
+            else
+            {
+                Console.WriteLine("Dead");
+            }
+
+
+            if ((age < 10) || (age < 13))
+            {
+                Console.WriteLine("Robal");
+            }
+
+            Console.WriteLine("!true = " + (!true));
+
+
+
 
 
             Console.ReadLine();
