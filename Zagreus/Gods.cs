feb3b1190def77746f4d0022eeea9ba5db1cdd60
Lindow <das@umbraco.dk>
2022-08-30 23:26:23 -0400

    5 static method and fields inside of a class

---
+++ Zagreus/Gods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Zagreus
+{
+    internal class God
+    {
+        public string name;
+        public string title;
+        public string affiliation;
+
+        public God()
+        {
+            name = "Nameless god";
+            title = "of xxx";
+            affiliation = "has no friends :(";
+            numOfGods++;
+        }
+
+        public God(string name = "Nameless god",
+            string title = "titleless",
+            string affiliation = "has no friends :(")
+        {
+            this.name = name;
+            this.title = title;
+            this.affiliation = affiliation;
+            numOfGods++;
+        }
+
+        public void GetTitle()
+        {
+            Console.WriteLine("{0} is called the {1}",
+                name, title);
+        }
+
+        static int numOfGods = 0;
+
+        public static int GetNumOfGods()
+        {
+            return numOfGods;
+        }
+
+    }
+}
--- Zagreus/Program.cs
+++ Zagreus/Program.cs
@@ -12,45 +12,22 @@ namespace Zagreus
     {
         static void Main(string[] args)
         {
-            InternalArms Varatha;
-            Varatha.damage = 9;
-            Varatha.speed = 5;
-            Console.WriteLine("Rank score of Varatha : {0}", Varatha.RankScore());
+            God Zagreus = new God()
+            {
+                name = "Zagreus",
+                title = "Prince of the Underworld",
+                affiliation = "House of Hades"
+            };
 
+            Zagreus.GetTitle();
 
-            InternalArms Stygius = new InternalArms(4, 10);
+            Console.WriteLine("{0} is called the {1} and he's a part of the {2}\nThere is {3} god",
+                Zagreus.name, Zagreus.title, Zagreus.affiliation, God.GetNumOfGods());
 
-            Console.WriteLine("Rank score of Stygius : {0}", Stygius.RankScore());
-
-            Varatha = Stygius;
-            Stygius.damage = 5;
-
-            Console.WriteLine("Rank score of Stygius's dmg : {0}", Stygius.damage);
 
 
             Console.ReadLine();
         }
-
-        struct InternalArms
-        {
-            public double damage;
-            public double speed;
-
-            public InternalArms(double d,
-                double s)
-            {
-                damage = d;
-                speed = s;
-            }
-
-            public double RankScore()
-            {
-                return damage * speed;
-            }
-
-
-        }
-
 
     }
 }
